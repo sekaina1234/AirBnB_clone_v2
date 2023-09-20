6625dc2d0fe43caf5d7ed7ebcc538e65521325fb
sekaina1234 <soukainasaoud54@gmail.com>
2023-09-20 14:10:47 -0700

    2. Console improvements

--- console.py
+++ console.py
@@ -14,22 +14,22 @@ def do_create(self, args):
         print("** class doesn't exist **")
         return
 
-    # Create a dictionary to store attribute key-value pairs
+
     attr_dict = {}
 
-    # Iterate through the remaining arguments (key=value pairs)
+
     for arg in arg_list[1:]:
-        # Split each argument into key and value
+
         key_value = arg.split('=')
         if len(key_value) != 2:
             continue
         key, value = key_value
 
-        # Remove double quotes from string values and replace underscores with spaces
+
         if value.startswith('"') and value.endswith('"'):
             value = value[1:-1].replace('_', ' ')
 
-        # Try to convert the value to int or float if possible
+
         try:
             if '.' in value:
                 value = float(value)
@@ -38,10 +38,9 @@ def do_create(self, args):
         except ValueError:
             pass
 
-        # Add the key-value pair to the attribute dictionary
+
         attr_dict[key] = value
 
-    # Create a new instance of the class with the attribute dictionary
     new_instance = self.classes[class_name](**attr_dict)
     new_instance.save()
     print(new_instance.id)
