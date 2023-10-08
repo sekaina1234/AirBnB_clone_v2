3b128955f358aaa063433a5fcc55d52ca53a945f
sekaina1234 <soukainasaoud54@gmail.com>
2023-10-08 04:53:21 -0700

    2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,6 @@
 Fabric script that distributes an archive to your web servers,
 using the function do_deploy
 """
-
 from fabric.api import local, put, run, env
 from os.path import exists
 from datetime import datetime
@@ -17,37 +16,55 @@ def do_deploy(archive_path):
     """
     Distributes an archive to the web servers and deploys it
     """
-    if not exists(archive_path):
+from fabric.operations import local, put, run
+from datetime import datetime as d
+from fabric.api import *
+
+env.hosts = ['34.74.120.150', '54.173.196.75']
+
+
+def do_pack():
+    """ generates a .tgz archive """
+    name = "versions/web_static_" + str(d.now().year)
+    name += str(d.now().month) + str(d.now().day) + str(d.now().hour)
+    name += str(d.now().minute) + str(d.now().second) + ".tgz"
+    result = local("mkdir -p versions; tar -cvzf \"%s\" web_static" % name)
+    if result.failed:
+        return NULL
+    else:
+        return name
+
+
+def do_deploy(archive_path):
+    """ uploads the archive to servers """
+    destination = "/tmp/" + archive_path.split("/")[-1]
+    result = put(archive_path, "/tmp/")
+    if result.failed:
         return False
-
-    try:
-        # Upload the archive to the /tmp/ directory of the web server
-        put(archive_path, '/tmp/')
-
-        # Extract the archive to the folder
-        archive_filename = archive_path.split('/')[-1]
-        archive_name_no_extension = archive_filename.split('.')[0]
-        release_folder = "/data/web_static/releases/{}".format(
-            archive_name_no_extension)
-        run("mkdir -p {}".format(release_folder))
-        run("tar -xzf /tmp/{} -C {}".format(archive_filename, release_folder))
-
-        # Delete the archive from the web server
-        run("rm /tmp/{}".format(archive_filename))
-
-        # Move the contents of the release folder to the parent directory
-        run("mv {}/web_static/* {}".format(release_folder, release_folder))
-
-        # Remove the now empty web_static folder
-        run("rm -rf {}/web_static".format(release_folder))
-
-        # Delete the old symbolic link /data/web_static/current
-        run("rm -rf /data/web_static/current")
-
-        # Create a new symbolic link to the new version of your code
-        run("ln -s {} /data/web_static/current".format(release_folder))
-
-        print("New version deployed!")
-        return True
-    except Exception:
+    filename = archive_path.split("/")[-1]
+    f = filename.split(".")[0]
+    directory = "/data/web_static/releases/" + f
+    run_res = run("mkdir -p \"%s\"" % directory)
+    if run_res.failed:
         return False
+    run_res = run("tar -xzf %s -C %s" % (destination, directory))
+    if run_res.failed:
+        return False
+    run_res = run("rm %s" % destination)
+    if run_res:
+        return False
+    web = directory + "/web_static/*"
+    run_res = run("mv %s %s" % (web, directory))
+    if run_res.failed:
+        return False
+    web = web[0:-2]
+    run_res = run("rm -rf %s" % web)
+    if run_res.failed:
+        return False
+    run_res = run("rm -rf /data/web_static/current")
+    if run_res.failed:
+        return False
+    run_res = run("ln -s %s /data/web_static/current" % directory)
+    if run_res.failed:
+        return False
+    return True
