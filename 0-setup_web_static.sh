e13cdd5476d391e239d946a83a56e3f033d015d9
sekaina1234 <soukainasaoud54@gmail.com>
2023-10-08 04:30:09 -0700

    0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,37 +1,15 @@
 #!/usr/bin/env bash
-
-# Install Nginx if not already installed
-if ! command -v nginx &> /dev/null; then
-    apt-get update -y
-    apt-get install -y nginx
+# Update package list and install Nginx if not already installed
+if ! which nginx > /dev/null 2>&1; then
+sudo apt-get -y update
+sudo apt-get -y install nginx
 fi
-
-# Create necessary directories if they don't exist
-mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared
-
-# Create a fake HTML file
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
-
-# Create a symbolic link and update ownership
-ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration
-config_file="/etc/nginx/sites-available/default"
-if grep -q "location /hbnb_static {" "$config_file"; then
-    sed -i '/location \/hbnb_static {/!b;n;c\        alias /data/web_static/current/;' "$config_file"
-else
-    echo "location /hbnb_static {
-        alias /data/web_static/current/;
-    }" >> "$config_file"
-fi
-
-# Restart Nginx
-service nginx restart
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+myc="\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/\;\n\t}\n"
+st="server {"
+sudo sed -i "s/^$st/$st$myc/" /etc/nginx/sites-enabled/default
+sudo service nginx start
