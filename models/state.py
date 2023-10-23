bf9aa334dd4f0d8062532fb8324a7b7bbfe890d0
sekaina1234 <soukainasaoud54@gmail.com>
2023-10-23 05:44:06 -0700

    state.py

--- models/state.py
+++ models/state.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
-""" State module """
-
+"""This is the state class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.city import City
+
 
 class State(BaseModel, Base):
-    """ State class """
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = "states"
-
     name = Column(String(128), nullable=False)
-    
-    # For DBStorage, create a relationship with City
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    cities = relationship("City", cascade="all,delete", backref="state")
+
+    @property
+    def cities(self):
+        """  getter attribute cities that returns the list of
+        City instances with state_id equals to the current State.id """
+        from models import storage
+        allcities = storage.all(City)
+        c = []
+        for k, v in allcities.items():
+            if v.state_id == self.id:
+                c.append(v)
+        return c
